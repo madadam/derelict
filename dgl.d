002dd7b836d895fb034843b858583d859f1d659e
aldacron <aldacron@e20cc5ce-cdd9-0310-a41c-c650352df26b>
2010-03-27 16:10:52 +0000

    [Derelict2] * modified dgl.d to work with both D1 & D2 Phobos * inc/compiler_inc.mak isn't really modified... it just looks that way after I updated from the Linux commits. No clue why.
    git-svn-id: http://svn.dsource.org/projects/derelict/branches/Derelict2@451 e20cc5ce-cdd9-0310-a41c-c650352df26b

--- dgl.d
+++ dgl.d
@@ -1,9 +1,19 @@
 module dgl;
 
-pragma(lib, "lib\\DerelictGL.lib");
-pragma(lib, "lib\\DerelictGLU.lib");
-pragma(lib, "lib\\DerelictSDL.lib");
-pragma(lib, "lib\\DerelictUtil.lib");
+version(Windows)
+{
+	pragma(lib, "lib\\DerelictGL.lib");
+	pragma(lib, "lib\\DerelictGLU.lib");
+	pragma(lib, "lib\\DerelictSDL.lib");
+	pragma(lib, "lib\\DerelictUtil.lib");
+}
+else
+{
+	pragma(lib, "lib/DerelictGL.a");
+	pragma(lib, "lib/DerelictGLU.a");
+	pragma(lib, "lib/DerelictSDL.a");
+	pragma(lib, "lib/DerelictUtil.a");
+}
 
 import derelict.sdl.sdl;
 //import derelict.sdl.image;
@@ -12,10 +22,16 @@ import derelict.sdl.sdl;
 //import derelict.sdl.ttf;
 import derelict.opengl.gl;
 import derelict.opengl.glu;
-import std.string;
+import derelict.util.compat;
 import std.stdio;
 
-void dumpver(char[] sdlLib, SDL_version* ver)
+version(D_Version2) {}
+else
+{
+	alias writefln writeln;
+}
+
+void dumpver(string sdlLib, CSDLVERPTR ver)
 {
 	writefln("%s version: %d.%d.%d", sdlLib, ver.major, ver.minor, ver.patch);
 }
@@ -32,7 +48,7 @@ void main()
 	
 	if(SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		throw new Exception("Couldn't init SDL: " ~ .toString(SDL_GetError()));
+		throw new Exception("Couldn't init SDL: " ~ toDString(SDL_GetError()));
 	}	
 	scope(exit) 
 	{
@@ -46,10 +62,10 @@ void main()
 	
 	if(SDL_SetVideoMode(1024, 768, 0, SDL_OPENGL) == null)
 	{
-		throw new Exception("Failed to set video mode: " ~ toString(SDL_GetError()));
+		throw new Exception("Failed to set video mode: " ~ toDString(SDL_GetError()));
 	}
 	
-	SDL_version* sdlver = SDL_Linked_Version();
+	CSDLVERPTR sdlver = SDL_Linked_Version();
 	dumpver("SDL", sdlver);
 /*	
 	sdlver = IMG_Linked_Version();
@@ -67,20 +83,20 @@ void main()
 	GLVersion ver = DerelictGL.loadExtendedVersions(GLVersion.GL21);
 	writefln("Max GL version = %s", DerelictGL.versionToString(ver));
 	
-	char[] glver = toString(glGetString(GL_VERSION));
+	string glver = toDString(glGetString(GL_VERSION));
 	writefln("GL version string = %s", glver);
 	
-	char[] gluver = toString(gluGetString(GLU_VERSION));
+	string gluver = toDString(gluGetString(GLU_VERSION));
 	writefln("GLU version string = %s", gluver);
 	
 	DerelictGL.loadExtensions();
 
-	writefln("Loaded OpenGL Extensions:");
+	writeln("Loaded OpenGL Extensions:");
 	string[] extlist = DerelictGL.loadedExtensionNames;
 	foreach(s; extlist)
 		writefln("\t%s", s);
 		
-	writefln("Not Loaded OpenGL Extensions:");
+	writeln("Not Loaded OpenGL Extensions:");
 	extlist = DerelictGL.notLoadedExtensionNames;
 	foreach(s; extlist)
 		writefln("\t%s", s);
